82747f30240aa5e974344fb8878e541acd852bf7
michalwie1 <michaliwieder@gmail.com>
2025-12-29 10:04:30 +0200

    Support reviews aggregations

---
+++ api/review/review.routes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+import { requireAuth } from '../../middlewares/requireAuth.middleware.js'
+import { log } from '../../middlewares/logger.middleware.js'
+
+import {addReview, getReviews, deleteReview} from './review.controller.js'
+
+// const router = express.Router()
+export const reviewRoutes = express.Router()
+
+
+reviewRoutes.get('/', log, getReviews)
+reviewRoutes.post('/',  log, requireAuth, addReview)
+reviewRoutes.delete('/:id',  requireAuth, deleteReview)
+
+// export const reviewRoutes = router
