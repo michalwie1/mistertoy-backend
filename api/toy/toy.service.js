f58810cd0a2fa5c3f62f7a9591253a0201e9001a
michalwie1 <michaliwieder@gmail.com>
2025-12-29 09:34:25 +0200

    Init backend

---
+++ api/toy/toy.service.js
@@ -0,0 +1,127 @@
+import { ObjectId } from 'mongodb'
+
+import { dbService } from '../../services/db.service.js'
+import { logger } from '../../services/logger.service.js'
+import { utilService } from '../../services/util.service.js'
+
+export const toyService = {
+	remove,
+	query,
+	getById,
+	add,
+	update,
+	addToyMsg,
+	removeToyMsg,
+}
+
+async function query(filterBy = { txt: '' }) {
+	try {
+		const criteria = {
+			name: { $regex: filterBy.txt, $options: 'i' },
+		}
+		const collection = await dbService.getCollection('toy')
+		var toys = await collection.find(criteria).toArray()
+		return toys
+	} catch (err) {
+		logger.error('cannot find toys', err)
+		throw err
+	}
+}
+
+async function getById(toyId) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		const toy = await collection.findOne({ _id: ObjectId.createFromHexString(toyId) })
+		toy.createdAt = toy._id.getTimestamp()
+		return toy
+	} catch (err) {
+		logger.error(`while finding toy ${toyId}`, err)
+		throw err
+	}
+}
+
+async function remove(toyId) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		const { deletedCount } = await collection.deleteOne({ _id: ObjectId.createFromHexString(toyId) })
+        return deletedCount
+	} catch (err) {
+		logger.error(`cannot remove toy ${toyId}`, err)
+		throw err
+	}
+}
+
+async function add(toy) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		await collection.insertOne(toy)
+		return toy
+	} catch (err) {
+		logger.error('cannot insert toy', err)
+		throw err
+	}
+}
+
+async function update(toy) {
+	try {
+		const { name, price, labels } = toy
+		const toyToUpdate = {
+			name,
+			price,
+			labels,
+		}
+		console.log('toy._id:', toy._id)
+
+		const collection = await dbService.getCollection('toy')
+		// await collection.updateOne({ _id: ObjectId.createFromHexString(toy._id) }, { $set: toyToUpdate })
+		await collection.updateOne(
+	{ _id: new ObjectId(toy._id) },
+	{ $set: toyToUpdate }
+)
+		return toy
+	} catch (err) {
+		logger.error(`cannot update toy ${toy._id}`, err)
+		throw err
+	}
+}
+
+// async function update(toy) {
+// 	try {
+// 		const { name, price, labels } = toy
+// 		const toyToUpdate = {
+// 			name,
+// 			price,
+// 			labels,
+// 		}
+// 		const collection = await dbService.getCollection('toy')
+// 		await collection.updateOne({ _id: ObjectId.createFromHexString(toy._id) }, { $set: toyToUpdate })
+// 		return toy
+// 	} catch (error) {
+// 		loggerService.error(`cannot update toy ${toy._id}`, error)
+// 		throw error
+// 	}
+// }
+
+async function addToyMsg(toyId, msg) {
+	try {
+		msg.id = utilService.makeId()
+
+		const collection = await dbService.getCollection('toy')
+		await collection.updateOne({ _id: ObjectId.createFromHexString(toyId) }, { $push: { msgs: msg } })
+		return msg
+	} catch (err) {
+		logger.error(`cannot add toy msg ${toyId}`, err)
+		throw err
+	}
+}
+
+async function removeToyMsg(toyId, msgId) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		await collection.updateOne({ _id: ObjectId.createFromHexString(toyId) }, { $pull: { msgs: { id: msgId }}})
+		return msgId
+	} catch (err) {
+		logger.error(`cannot add toy msg ${toyId}`, err)
+		throw err
+	}
+}
---
+++ public/services/user.service.js
@@ -0,0 +1,78 @@
+import { storageService } from './async-storage.service.js'
+import { httpService } from './http.service.js'
+
+const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
+
+export const userService = {
+    login,
+    logout,
+    signup,
+    getLoggedinUser,
+    saveLocalUser,
+    query,
+    getById,
+    remove,
+    update,
+}
+
+window.userService = userService
+
+
+function query() {
+    // return storageService.query('user')
+    return httpService.get(`user`)
+}
+
+
+async function getById(userId) {
+    // const user = await storageService.get('user', userId)
+    const user = await httpService.get(`user/${userId}`)
+    return user
+}
+function remove(userId) {
+    // return storageService.remove('user', userId)
+    return httpService.delete(`user/${userId}`)
+}
+
+async function update(user) {
+    // await storageService.put('user', user)
+    user = await httpService.put(`user/${user._id}`, user)
+    // Handle case in which admin updates other user's details
+    if (getLoggedinUser()._id === user._id) saveLocalUser(user)
+    return user
+}
+
+async function login(userCred) {
+    // const users = await storageService.query('user')
+    // const user = users.find(user => user.username === userCred.username)
+    const user = await httpService.post('auth/login', userCred)
+    if (user) {
+        return saveLocalUser(user)
+    }
+}
+async function signup(userCred) {
+    if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
+    // const user = await storageService.post('user', userCred)
+    const user = await httpService.post('auth/signup', userCred)
+    return saveLocalUser(user)
+}
+async function logout() {
+    sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+    return await httpService.post('auth/logout')
+}
+
+
+
+function saveLocalUser(user) {
+    user = {_id: user._id, fullname: user.fullname, imgUrl: user.imgUrl, isAdmin: user.isAdmin}
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
+    return user
+}
+
+function getLoggedinUser() {
+    return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
+}
+
+
+
+
