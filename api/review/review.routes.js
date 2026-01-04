d82af25af8b6b7e72d0fb3048ab2e14eff9951fc
michalwie1 <michaliwieder@gmail.com>
2026-01-04 11:33:23 +0200

    Fix only admin can remove a review

--- api/review/review.routes.js
+++ api/review/review.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { requireAuth } from '../../middlewares/requireAuth.middleware.js'
+import { requireAuth, requireAdmin } from '../../middlewares/requireAuth.middleware.js'
 import { log } from '../../middlewares/logger.middleware.js'
 
 import {addReview, getReviews, deleteReview} from './review.controller.js'
@@ -11,6 +11,6 @@ export const reviewRoutes = express.Router()
 
 reviewRoutes.get('/', log, getReviews)
 reviewRoutes.post('/',  log, requireAuth, addReview)
-reviewRoutes.delete('/:id',  requireAuth, deleteReview)
+reviewRoutes.delete('/:id',  requireAdmin, deleteReview)
 
 // export const reviewRoutes = router
